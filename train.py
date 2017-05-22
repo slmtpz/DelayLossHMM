f4572a681614dc1b2d620979a7f67248aac14c5f
slmtpz <seleme94@hotmail.com>
2017-05-23 00:05:52 +0300

    milestone comments are written to indicate algorithm and some helpers are written

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from DelayLossHMM import DelayLossHMM
 import numpy as np
 from scipy.stats import gamma
-from
+from sympy.functions.special.delta_functions import DiracDelta
 
 ### Setting Input Parameters
 
@@ -12,7 +12,7 @@ N = 2
 y = [1,1,1] # y1, y2, .. , yN
 
 # stopping parameter
-epsilon = 1
+epsilon = 1  # TODO: set it to the right epsilon based on convergence rate for parameter likelihoods
 
 ### Initialization Step
 
@@ -30,11 +30,40 @@ v = np.ones(N)
 
 ### Iterative Step
 
-new, old = 1, 1
-while new - old > epsilon:
+new, old = 1, 1  # likelihood of parameters, we want the likelihoods for parameters to converge as much as possible
+while new - old > epsilon: # check if converged
+    ### EM algorithm
+    # a = hmm.forward()
+    # b = hmm.backward()
+    # ro = ...
+    # update parameters
+    # find new likelihood, new = sum(a)
     pass
 
+### Viterbi
+
+# now that parameters learned, time to find best sequence of states for hidden states
+# hmm.viterbi()
+
+### Results, graphs, prediction(?)
+
+### Helpers
+# may be moved to hmm class or some other helper py file
 
 # state conditioned delay probability density function (pdf)
 def f(i, t):
     return gamma.pdf(t / v[i], gammas[i]) / v[i]
+
+
+# b is later 'corrected' to b_modified in the paper, # TODO: idk if we should use b or use only b_mod yet.
+def b(i, t):
+    return p[i] * f(i, t) + (1 - p[i]) * DiracDelta(t + 1)
+
+
+def b_mod(i, t):
+    return p[i] * f(i, t) + (1 - p[i]) * g(t)
+
+
+def g(t):
+    delta = 0.01 # set to be an arbitrary small number # TODO: change the value ?
+    return 1 / 2 * delta if -1-delta <= t <= -1+delta else 0
