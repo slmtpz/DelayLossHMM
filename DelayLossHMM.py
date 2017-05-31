28bfdf81fb0829f2f81b9267a1b2bc0a30cfb5c3
slmtpz <seleme94@hotmail.com>
2017-05-31 15:49:53 +0300

    modifications

--- DelayLossHMM.py
+++ DelayLossHMM.py
@@ -1,18 +1,54 @@
 import numpy as np
+from scipy.stats import gamma
+from sympy.functions.special.delta_functions import DiracDelta
 
 
 class DelayLossHMM(object):
-    def __init__(self, pi, A, B):
-        # p(x_0)
-        self.pi = pi
-        # p(x_k|x_{k-1})
-        self.A = A
-        # p(y_k|x_{k})
-        self.B = B
-        # Number of possible latent states at each time
-        self.S = pi.shape[0]
-        # Number of possible observations at each time
-        self.R = B.shape[0]
-        self.logB = np.log(self.B)
-        self.logA = np.log(self.A)
-        self.logpi = np.log(self.pi)
+    def __init__(self, N, y):
+        self.N = N  # number of states
+        self.y = y  # training sequence TODO: may be moved to a train function
+        ### Initialization Step
+
+        # transition probability matrix with each index being 1/N
+        self.A = np.ones([N, N]) / N
+
+        # loss probability vector notating probability of packet loss for each state
+        self.p = np.ones(N) / 2
+
+        # starting gamma parameters for gamma distribution
+        self.gammas = np.arange(1, N + 1) * np.max(y) / (N + 1)
+
+        # d = y*v, sigma^2 = y*v^2, later to be used to derive d (mean delay) and sigma (variance)
+        self.v = np.ones(N)
+
+    def forward_backward(self):
+        log_alpha = [1, 1, 1]
+        log_beta = [1, 1, 1]
+        log_ro = [1, 1, 1]
+        return log_alpha, log_beta, log_ro
+
+    def viterbi(self):
+        seq = [1, 1, 1]
+        return seq
+
+
+    ### Helpers
+    # may be moved to hmm class or some other helper py file
+
+    # state conditioned delay probability density function (pdf)
+    def f(self, i, t):
+        return gamma.pdf(t / self.v[i], self.gammas[i]) / self.v[i]
+
+
+    # b is later 'corrected' to b_modified in the paper, # TODO: idk if we should use b or use only b_mod yet.
+    def b(self, i, t):
+        return self.p[i] * self.f(i, t) + (1 - self.p[i]) * DiracDelta(t + 1)
+
+
+    def b_mod(self, i, t):
+        return self.p[i] * self.f(i, t) + (1 - self.p[i]) * g(t)
+
+
+    def g(self, t):
+        delta = 0.01 # set to be an arbitrary small number # TODO: change the value ?
+        return 1 / 2 * delta if -1-delta <= t <= -1+delta else 0
--- train.py
+++ train.py
@@ -1,7 +1,4 @@
 from DelayLossHMM import DelayLossHMM
-import numpy as np
-from scipy.stats import gamma
-from sympy.functions.special.delta_functions import DiracDelta
 
 ### Setting Input Parameters
 
@@ -9,61 +6,30 @@ from sympy.functions.special.delta_functions import DiracDelta
 N = 2
 
 # training sequence
-y = [1,1,1] # y1, y2, .. , yN
+y = [1, 1, 1]  # y1, y2, .. , yN
 
 # stopping parameter
 epsilon = 1  # TODO: set it to the right epsilon based on convergence rate for parameter likelihoods
 
+
 ### Initialization Step
+hmm = DelayLossHMM(N, y)
 
-# transition probability matrix with each index being 1/N
-A = np.ones([N, N]) / N
-
-# loss probability vector notating probability of packet loss for each state
-p = np.ones(N) / 2
-
-# starting gamma parameters for gamma distribution
-gammas = np.arange(1, N+1) * np.max(y) / (N + 1)
-
-# d = y*v, sigma^2 = y*v^2, later to be used to derive d (mean delay) and sigma (variance)
-v = np.ones(N)
 
 ### Iterative Step
 
 new, old = 1, 1  # likelihood of parameters, we want the likelihoods for parameters to converge as much as possible
+### EM algorithm
 while new - old > epsilon: # check if converged
-    ### EM algorithm
-    # a = hmm.forward()
-    # b = hmm.backward()
-    # ro = ...
-    # update parameters
-    # find new likelihood, new = sum(a)
+    log_alpha, log_beta, log_ro = hmm.forward_backward()
+    # update A,p,d,sigma
+    # likelihood = sum(alpha)
     pass
 
 ### Viterbi
 
 # now that parameters learned, time to find best sequence of states for hidden states
-# hmm.viterbi()
+seq = hmm.viterbi()
 
 ### Results, graphs, prediction(?)
 
-### Helpers
-# may be moved to hmm class or some other helper py file
-
-# state conditioned delay probability density function (pdf)
-def f(i, t):
-    return gamma.pdf(t / v[i], gammas[i]) / v[i]
-
-
-# b is later 'corrected' to b_modified in the paper, # TODO: idk if we should use b or use only b_mod yet.
-def b(i, t):
-    return p[i] * f(i, t) + (1 - p[i]) * DiracDelta(t + 1)
-
-
-def b_mod(i, t):
-    return p[i] * f(i, t) + (1 - p[i]) * g(t)
-
-
-def g(t):
-    delta = 0.01 # set to be an arbitrary small number # TODO: change the value ?
-    return 1 / 2 * delta if -1-delta <= t <= -1+delta else 0
